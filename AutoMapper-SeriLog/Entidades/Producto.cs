b09398ec4eda4f0b2fc8e50c1608c349d0421c86
edgar.garcia <edgar.garcia@orbis.com.pe>
2021-06-23 19:26:53 -0500

    ceracion carpeta entidad

---
+++ AutoMapper-SeriLog/Entidades/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMapper_SeriLog.Entidades
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public double Descripcion { get; set; }
+        public double Precio { get; set; }
+    }
+}
