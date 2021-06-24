9e1ae63af5b8780ea5e8fa4ae519407036474cee
edgar.garcia <edgar.garcia@orbis.com.pe>
2021-06-23 19:54:58 -0500

    Creacion entidad Usuario

---
+++ AutoMapper-SeriLog/Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMapper_SeriLog.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int IdPerfil { get; set; }
+    }
+}
